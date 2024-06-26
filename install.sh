bdcb18a19c140775970b74cf0f7317403898aee7
NextPr0gram <anafkarim@proton.me>
2024-06-26 22:13:24 +0100

    fix: improve script

--- install.sh
+++ install.sh
@@ -14,39 +14,69 @@
 
 dots="$HOME/Dots-Hyprland"
 
+# Function to handle errors
+handle_error() {
+    echo "Error on line $1"
+    exit 1
+}
+
+# Trap errors
+trap 'handle_error $LINENO' ERR
+
 
 # Installing Arch and AUR packages
 echo "Updating system and installing base-devel package group"
 sudo pacman -Syu --needed base-devel
 
-echo "Installing paru"
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
 
 echo "Installing Arch packages from $dots/arch-packages.txt"
-sudo pacman -S - < "$dots/arch-packages.txt"
+if [[ -f "$dots/arch-packages.txt" ]]; then
+    sudo pacman -S - < "$dots/arch-packages.txt"
+else
+    echo "File $dots/arch-packages.txt not found."
+    exit 1
+fi
+
+echo "Installing paru"
+if [[ ! -d "paru" ]]; then
+    git clone https://aur.archlinux.org/paru.git
+else
+    echo "Directory paru already exists. Skipping clone."
+fi
+
+cd paru
+makepkg -si
+cd ..
 
 echo "Installing AUR packages from $dots/aur-packages.txt"
-paru -S - < "$dots/aur-packages.txt"
+if [[ -f "$dots/aur-packages.txt" ]]; then
+    paru -S - < "$dots/aur-packages.txt"
+else
+    echo "File $dots/aur-packages.txt not found."
+    exit 1
+fi
 
 # Create user directories
 xdg-user-dirs-update
 
-
 # Creating symbolic links
-ln -s $dots/ags ~/.config
-ln -s $dots/cava ~/.config
-ln -s $dots/fastfetch ~/.config
-ln -s $dots/fontconfig ~/.config
-ln -s $dots/gtk-3.0 ~/.config
-ln -s $dots/gtk-4.0 ~/.config
-ln -s $dots/hypr ~/.config
-ln -s $dots/kitty ~/.config
-ln -s $dots/nwg-look ~/.config
-ln -s $dots/rofi ~/.config
-ln -s $dots/swww ~/.config
-ln -s $dots/xsettingsd ~/.config
+config_dirs=(
+    "ags"
+    "cava"
+    "fastfetch"
+    "fontconfig"
+    "gtk-3.0"
+    "gtk-4.0"
+    "hypr"
+    "kitty"
+    "nwg-look"
+    "rofi"
+    "swww"
+    "xsettingsd"
+)
+
+for dir in "${config_dirs[@]}"; do
+    ln -s "$dots/$dir" "$HOME/.config/$dir"
+done
 
 echo "Installation complete! Please reboot now"
-
