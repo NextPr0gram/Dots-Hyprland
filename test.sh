61dba9d808ca1ce27563b0deb4d225f6cb97078a
NextPr0gram <anafkarim@proton.me>
2024-09-22 21:50:21 +0100

    add test.sh

--- test.sh
+++ test.sh
@@ -6,12 +6,6 @@ for dir in $config_dirs; do
     target_dir="$HOME/.config/$base_dir"
     
     echo "Processing $dir -> $target_dir"
-
-    # Check if the directory or symlink already exists and remove it if it does
-    if [ -d "$target_dir" ] || [ -L "$target_dir" ]; then
-        echo "Directory or symlink $target_dir exists. Deleting it..."
-        rm -rf "$target_dir" || { echo "Failed to delete $target_dir"; exit 1; }
-    fi
     
     # Create a symbolic link
     ln -s "$dir" "$target_dir" || { echo "Failed to create symbolic link for $base_dir"; exit 1; }
