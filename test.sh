38beeee3c181efd7e188005fc87aa351abb256cc
NextPr0gram <anafkarim@proton.me>
2024-09-22 21:46:34 +0100

    add test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,20 @@
-#!/bin/bash
-config_dirs=$(find "config" -mindepth 1 -maxdepth 1 -type d)
+# Create symbolic links
+config_dirs=$(find "$(pwd)/config" -mindepth 1 -maxdepth 1)
 
 for dir in $config_dirs; do
     base_dir=$(basename "$dir")
-    echo "$base_dir"
+    target_dir="$HOME/.config/$base_dir"
+    
+    echo "Processing $dir -> $target_dir"
+
+    # Check if the directory or symlink already exists and remove it if it does
+    if [ -d "$target_dir" ] || [ -L "$target_dir" ]; then
+        echo "Directory or symlink $target_dir exists. Deleting it..."
+        rm -rf "$target_dir" || { echo "Failed to delete $target_dir"; exit 1; }
+    fi
+    
+    # Create a symbolic link
+    ln -s "$dir" "$target_dir" || { echo "Failed to create symbolic link for $base_dir"; exit 1; }
+    echo "Symbolic link created: $target_dir -> $dir"
 done
+
